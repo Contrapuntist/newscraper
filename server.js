e4af430aaaf6693433d2cd52cec0d87d885aa342
Contrapuntist <contrapuntist@gmail.com>
2017-09-17 21:42:44 -0500

    scrape working, finally

--- server.js
+++ server.js
@@ -50,35 +50,35 @@ app.get("/scrape", function(req, res) {
     var $ = cheerio.load(html);
     // console.log(html);
         // Defining content to scrape from site
-        $("div.page-column article header").each(function(i, element) {
+        $("div.page-column article").each(function(i, element) {
         console.log('in section fetching');
             // Save an empty result object
             var result = {};
     
             // Add the text and href of every link, and save them as properties of the result object
             // result.section = $(this).html();
-            result.title = $(this).first('h2 a').text();
-            result.link = $(this).filter('h2 a').attr("href");
-            // result.summary = $(this).children('p').text();
+            result.title = $(this).find('h2 a').text();
+            result.link = 'https://theconversation.com' + $(this).find('h2 a').attr("href");
+            result.summary = $('div.content').first().text();
             
             console.log('***********************          Results Entry       *****************************************************');
             console.log(result);
 
             // Using our Article model, create a new entry
             // This effectively passes the result object to the entry (and the title and link)
-            // var entry = new Article(result);
+            var entry = new Article(result);
 
             // Now, save that entry to the db
-            // entry.save(function(err, doc) {
-            //     // Log any errors
-            //     if (err) {
-            //         console.log(err);
-            //     }
-            //     // Or log the doc
-            //     else {
-            //         console.log(doc);
-            //     }
-            // }); 
+            entry.save(function(err, doc) {
+                // Log any errors
+                if (err) {
+                    console.log(err);
+                }
+                // Or log the doc
+                else {
+                    console.log(doc);
+                }
+            }); 
         });
     });
     // Tell the browser that we finished scraping the text
